396ee49f5d1c4e4c9f740a9a30222785bbdbf91d
AdaParra <adapparrad@gmail.com>
2018-05-03 22:45:43 +0100

    Navigate to a new screen

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Startup Name Generator',
+      title: 'Startup Name',
       home: new RandomWords(),
     );
   }
@@ -20,32 +20,39 @@ class RandomWords extends StatefulWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
-  final _saved = new Set <WordPair>();
+
+  final _saved = new Set<WordPair>();
+
   final _biggerFont = const TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
-    //final wordPair = new WordPair.random();
-    //return (new Text(wordPair.asPascalCase));
-    return new Scaffold (
+    return new Scaffold(
       appBar: new AppBar(
-        title: new Text('Generador de Nombres'),
+        title: new Text('Startup AG'),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
     );
   }
-  Widget _buildSuggestions(){
+
+  Widget _buildSuggestions() {
     return new ListView.builder(
       padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i){
+      itemBuilder: (context, i) {
         if (i.isOdd) return new Divider();
+
         final index = i ~/ 2;
-        if (index >= _suggestions.length){
+        if (index >= _suggestions.length) {
           _suggestions.addAll(generateWordPairs().take(10));
         }
         return _buildRow(_suggestions[index]);
-      }
+      },
     );
   }
+
   Widget _buildRow(WordPair pair) {
     final alreadySaved = _saved.contains(pair);
     return new ListTile(
@@ -57,16 +64,48 @@ class RandomWordsState extends State<RandomWords> {
         alreadySaved ? Icons.favorite : Icons.favorite_border,
         color: alreadySaved ? Colors.red : null,
       ),
-      onTap: (){
-        setState(() {
-            if(alreadySaved){
+      onTap: () {
+        setState(
+          () {
+            if (alreadySaved) {
               _saved.remove(pair);
-            }
-            else{
+            } else {
               _saved.add(pair);
             }
-          });
+          },
+        );
       },
+    );
+  }
+
+  void _pushSaved(){
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (context){
+          final tiles = _saved.map(
+            (pair) {
+              return new ListTile(
+                title: new Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            }
+          );
+          final divided = ListTile
+            .divideTiles(
+              context: context,
+              tiles: tiles,
+            )
+            .toList();
+          return new Scaffold(
+          appBar: new AppBar(
+            title: new Text('Favoritos'),
+          ),
+          body: new ListView(children: divided),
+        );
+        }
+      )
     );
   }
 }
